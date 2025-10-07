362b803ebbb06429c0e9abb901c24dfea133b58d
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-07 12:23:02 -0700

    Fix unused var

--- src/middleware/xss-clean/index.ts
+++ src/middleware/xss-clean/index.ts
@@ -2,7 +2,7 @@ import { clean } from "./xss.js";
 import { type Request, type Response, type NextFunction } from "express";
 
 export default function xssSanitize() {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return (req: Request, _res: Response, next: NextFunction) => {
     if (req.body) req.body = clean(req.body);
     if (req.query) req.query = clean(req.query);
     if (req.params) req.params = clean(req.params);
