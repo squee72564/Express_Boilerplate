5bab0742321311d11727e402792f2b6b20754ffb
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-06 20:36:08 -0700

    Add eslint-plugin-prettier

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,28 +1,28 @@
 import { createLogger, format, transports } from "winston";
 import env from "../config/index.js";
 
-var logger = createLogger({
-  level: 'info',
+const logger = createLogger({
+  level: "info",
   format: format.combine(
-    format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-    format.errors({stack: true}),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
     format.splat(),
     format.json(),
   ),
-  defaultMeta: { service: 'express-template' },
+  defaultMeta: { service: "express-template" },
   transports: [
-    new transports.File({filename: 'express-template-error.log', level: 'error'}),
-    new transports.File({filename: 'express-template-error-combined.log'}),
+    new transports.File({
+      filename: "express-template-error.log",
+      level: "error",
+    }),
+    new transports.File({ filename: "express-template-error-combined.log" }),
   ],
 });
 
-if (env.NODE_ENV !== 'production') {
+if (env.NODE_ENV !== "production") {
   logger.add(
     new transports.Console({
-      format: format.combine(
-        format.colorize(),
-        format.simple(),
-      ),
+      format: format.combine(format.colorize(), format.simple()),
     }),
   );
 }
