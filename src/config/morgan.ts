6356dd9e4722b727e25af93f5c9afbbf8d54d16e
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-07 13:13:52 -0700

    change export style

--- src/config/morgan.ts
+++ src/config/morgan.ts
@@ -10,12 +10,17 @@ const getIpFormat = () => (env.NODE_ENV === "production" ? ":remote-addr - " : "
 const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms`;
 const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - message: :message`;
 
-export const successHandler = morgan(successResponseFormat, {
+const successHandler = morgan(successResponseFormat, {
   skip: (_req, res) => res.statusCode >= 400,
   stream: { write: (message) => logger.info(message.trim()) },
 });
 
-export const errorHandler = morgan(errorResponseFormat, {
+const errorHandler = morgan(errorResponseFormat, {
   skip: (_req, res) => res.statusCode < 400,
   stream: { write: (message) => logger.error(message.trim()) },
 });
+
+export default {
+  successHandler: successHandler,
+  errorHandler: errorHandler,
+};
