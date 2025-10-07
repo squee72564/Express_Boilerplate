88db7849619045487091e2d9878128d660b434ea
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-07 15:19:56 -0700

    Fix error with xss-clean middleware
    In Express v5 the req.params and req.query objects are readonly, so we have to modify them in place

--- src/middleware/xss-clean/index.ts
+++ src/middleware/xss-clean/index.ts
@@ -4,8 +4,20 @@ import { type Request, type Response, type NextFunction } from "express";
 export default function xssSanitize() {
   return (req: Request, _res: Response, next: NextFunction) => {
     if (req.body) req.body = clean(req.body);
-    if (req.query) req.query = clean(req.query);
-    if (req.params) req.params = clean(req.params);
+
+    if (req.query && Object.keys(req.query).length > 0) {
+      const cleanedquery = clean(req.query);
+      for (const key in cleanedquery) {
+        (req.query as Record<string, unknown>)[key] = cleanedquery[key];
+      }
+    }
+
+    if (req.params && Object.keys(req.params).length > 0) {
+      const cleanedparams = clean(req.params);
+      for (const key in cleanedparams) {
+        (req.query as Record<string, unknown>)[key] = cleanedparams[key];
+      }
+    }
 
     next();
   };
