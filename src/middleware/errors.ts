73c3961c6f0da30e05baf4bc79875cfe3a2ec25c
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-07 13:25:28 -0700

    Custom error converter and error handler middleware that works with custom ApiError class

---
+++ src/middleware/errors.ts
@@ -0,0 +1,45 @@
+import type { Response, Request, NextFunction } from "express";
+
+import httpStatus from "http-status";
+import env from "../config/index.js";
+import logger from "@/config/logger.js";
+import ApiError from "@/utils/ApiError.js";
+
+export const errorConverter = (
+  err: Error & { statusCode?: number },
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  let error = err;
+
+  if (!(error instanceof ApiError)) {
+    const statusCode = error.statusCode ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+
+    const message = error.message || httpStatus[statusCode];
+    error = new ApiError(statusCode, message, false, err.stack);
+  }
+  next(error);
+};
+
+export const errorHandler = (err: ApiError, _req: Request, res: Response, _next: NextFunction) => {
+  let { statusCode, message } = err;
+  if (env.NODE_ENV === "production" && !err.isOperational) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
+  }
+
+  res.locals.errorMessage = err.message;
+
+  const response = {
+    code: statusCode,
+    message,
+    ...(env.NODE_ENV === "development" && { stack: err.stack }),
+  };
+
+  if (env.NODE_ENV === "development") {
+    logger.error(err);
+  }
+
+  res.status(statusCode).send(response);
+};
