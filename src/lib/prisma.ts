673dd5f0289fa036365b85255c17e02250b774e1
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-07 23:34:42 -0700

    Add prisma schema and the prisma client file

---
+++ src/lib/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient as PrismaClientPostgres } from "../db/generated/prisma-postgres/index.js";
+import env from "../config/index.js";
+
+// It is recommended to use a connection pooler to manage database connections efficiently
+// Avoid instantiating PrismaClient globally in long lived environments
+// Instead create and dispose of the client per request to prevent exhausting db connections
+
+const getPrisma = () => new PrismaClientPostgres();
+
+const globalForPrisma = global as unknown as {
+  prisma: ReturnType<typeof getPrisma>;
+};
+
+const prisma = globalForPrisma.prisma || getPrisma();
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
