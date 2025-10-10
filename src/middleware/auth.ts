90076ea2eafb897639ddf2eaba5a93eb1d486ebb
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-09 18:24:41 -0700

    Auth middleware using better-auth
    On authentication we can also get the session and user info to attach to the request

---
+++ src/middleware/auth.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from "express";
+import { fromNodeHeaders } from "better-auth/node";
+import httpStatus from "http-status";
+
+import auth from "../lib/auth.js";
+import ApiError from "../utils/ApiError.js";
+import catchAsync from "../utils/catchAsync.ts";
+
+const authMiddleware = (_role?: string) =>
+  catchAsync(async (req: Request, _res: Response, next: NextFunction) => {
+    const headers = fromNodeHeaders(req.headers);
+    const result = await auth.api.getSession({
+      headers: headers,
+    });
+
+    if (!result) {
+      throw new ApiError(httpStatus.UNAUTHORIZED, "Unauthorized");
+    }
+
+    const { session, user } = result;
+    req.session = session;
+    req.user = user;
+
+    next();
+  });
+
+export default authMiddleware;
