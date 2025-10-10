4a1308d357803b9dbeca3006487bd44ceb997e09
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-09 18:22:54 -0700

    Fix validation middleware
    We should pass in the ZodObject that represents the schema for the request
    validate it and throw errors if something went wrong

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,43 +1,27 @@
-import { z, type ZodType } from "zod";
+import { z, ZodObject } from "zod";
 import type { Request, Response, NextFunction } from "express";
 import httpStatus from "http-status";
 import ApiError from "../utils/ApiError.js";
+import catchAsync from "@/utils/catchAsync.ts";
 
-interface ValidationSchema {
-  params?: ZodType;
-  query?: ZodType;
-  body?: ZodType;
-}
-
-const validate = (schema: ValidationSchema) => {
-  return (req: Request, _res: Response, next: NextFunction) => {
+const validate = (schema: ZodObject) =>
+  catchAsync(async (req: Request, _res: Response, next: NextFunction) => {
     try {
-      // Validate params
-      if (schema.params) {
-        req.params = schema.params.parse(req.params) as typeof req.params;
-      }
-
-      // Validate query
-      if (schema.query) {
-        req.query = schema.query.parse(req.query) as typeof req.query;
-      }
-
-      // Validate body
-      if (schema.body) {
-        req.body = schema.body.parse(req.body);
-      }
-
-      next();
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
     } catch (error) {
       if (error instanceof z.ZodError) {
         const errorMessage = error.issues
           .map((err) => `${err.path.join(".")}: ${err.message}`)
           .join(", ");
-        return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+        throw new ApiError(httpStatus.BAD_REQUEST, errorMessage);
       }
-      next(error);
+      throw new ApiError(httpStatus.BAD_REQUEST, "Validation error");
     }
-  };
-};
+    next();
+  });
 
 export default validate;
