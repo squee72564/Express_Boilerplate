32065e457284aa87f40b923756715c79f5d4ba84
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-11 16:28:49 -0700

    New template parameter for request type; default template parameters

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,10 +1,14 @@
 import { type Request, type Response, type NextFunction } from "express";
 
-type AsyncHandler<T = void> = (req: Request, res: Response, next: NextFunction) => Promise<T>;
+type AsyncHandler<U = Request, T = void> = (
+  req: U,
+  res: Response,
+  next: NextFunction
+) => Promise<T>;
 
 const catchAsync =
-  <T>(fn: AsyncHandler<T>) =>
-  (req: Request, res: Response, next: NextFunction) => {
+  <U = Request, T = void>(fn: AsyncHandler<U, T>) =>
+  (req: U, res: Response, next: NextFunction): void => {
     fn(req, res, next).catch(next);
   };
 
