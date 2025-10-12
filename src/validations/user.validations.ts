680492907eae8725c756b3310562c6a51e766e6d
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-12 11:30:21 -0700

    Change request validation for get user by id to match better auth api
    Remove old validation to get all users as we can reuse stuff from another similar validation/service

--- src/validations/user.validations.ts
+++ src/validations/user.validations.ts
@@ -1,21 +1,11 @@
 import { z } from "zod";
 
 const getUserById = z.object({
-  params: z.object({
+  query: z.object({
     id: z.uuid(),
   }),
 });
 
-const getAllUsers = z.object({
-  name: z.string().optional(),
-  email: z.email().optional(),
-  emailVerified: z.preprocess(
-    (val) => (val === "true" ? true : val === "false" ? false : undefined),
-    z.boolean().optional()
-  ),
-});
-
 export default {
   getUserById,
-  getAllUsers,
 };
