10d2626df66d828e98a839a3fee7b853cdbfe191
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-12 15:44:43 -0700

    Add use api cotrollers

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,23 +2,31 @@ import type { Response } from "express";
 
 import catchAsync from "../utils/catchAsync.js";
 import { userService } from "../services/index.js";
-import { GetUserRequest, ListUsersRequest } from "../types/user.types.js";
+import {
+  GetUserRequest,
+  GetUserWithSessionRequest,
+  ListPublicUsersRequest,
+} from "../types/user.types.js";
 
-const getUserById = catchAsync(async (req: GetUserRequest, res: Response) => {
-  const args = { ...req.query };
-
-  const user = await userService.getUserById(args, req);
-  res.status(200).json(user);
+const getUserWithSession = catchAsync(async (req: GetUserWithSessionRequest, res: Response) => {
+  return res.status(200).json({
+    user: req.user,
+    session: req.session,
+  });
 });
 
-const listUsers = catchAsync(async (req: ListUsersRequest, res: Response) => {
-  const args = { ...req.body };
+const getPublicUserById = catchAsync(async (req: GetUserRequest, res: Response) => {
+  const user = await userService.getPublicUserById(req.params.id);
+  return res.status(200).json({ user });
+});
 
-  const users = await userService.listUsers(args, req);
-  res.status(200).json(users);
+const listPublicUsers = catchAsync(async (req: ListPublicUsersRequest, res: Response) => {
+  const users = await userService.listPublicUsers(req.query);
+  return res.status(200).json({ users });
 });
 
 export default {
-  getUserById: getUserById,
-  listUsers: listUsers,
+  getUserWithSession: getUserWithSession,
+  getPublicUserById: getPublicUserById,
+  listPublicUsers: listPublicUsers,
 };
