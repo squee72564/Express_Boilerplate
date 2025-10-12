4f2444a5b3b2b44d1037de371aba6061ded29420
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-12 15:44:11 -0700

    Add user api request types

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,8 +1,10 @@
 import userValidations from "@/validations/user.validations.ts";
 import { ZodRequest } from "../utils/zodReqeust.ts";
 
-export type GetUserRequest = ZodRequest<typeof userValidations.getUserById>;
-export type GetUserArgs = GetUserRequest["query"];
+export type GetUserWithSessionRequest = ZodRequest<typeof userValidations.getUserWithSession>;
 
-export type ListUsersRequest = ZodRequest<typeof userValidations.listUsers>;
-export type ListUsersArgs = ListUsersRequest["body"];
+export type GetUserRequest = ZodRequest<typeof userValidations.getUserById>;
+export type GetUserArgs = GetUserRequest["params"];
+
+export type ListPublicUsersRequest = ZodRequest<typeof userValidations.listPublicUsers>;
+export type ListPublicUsersArgs = ListPublicUsersRequest["query"];
