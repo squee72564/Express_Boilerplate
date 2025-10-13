ef01b21580aeff970380211b7c0063dd82ce10f6
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-12 20:55:41 -0700

    Update index file

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,3 +1,4 @@
 import userValidations from "./user.validations.js";
+import adminValidations from "./admin.validations.js";
 
-export { userValidations };
+export { userValidations, adminValidations };
