2ffae8dff58ca033328cf76ea26e679f14b36004
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-12 20:55:16 -0700

    Use hard coded roles; update imports

--- src/routes/v1/admin.route.ts
+++ src/routes/v1/admin.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import auth from "../../middleware/auth.js";
 import validate from "../../middleware/validate.js";
-import adminValidations from "../../validations/admin.validations.js";
-import adminController from "../../controllers/admin.controller.js";
+import { adminValidations } from "../../validations/index.js";
+import { adminController } from "../../controllers/index.js";
+
+import { adminRoles } from "@/lib/permissions.ts";
 
 const router: Router = Router();
 
@@ -11,75 +13,75 @@ const userRoutes: Router = Router({ mergeParams: true });
 userRoutes
   .route("/")
   .patch(
-    auth(["admin"], { user: ["create"] }),
+    auth([...adminRoles], { user: ["create"] }),
     validate(adminValidations.UpdateUserRequestSchema),
     adminController.updateUser
   )
   .delete(
-    auth(["admin"], { user: ["delete"] }),
+    auth([...adminRoles], { user: ["delete"] }),
     validate(adminValidations.RemoveUserRequestSchema),
     adminController.removeUser
   );
 
 userRoutes.patch(
   "/role",
-  auth(["admin"], { user: ["set-role"] }),
+  auth([...adminRoles], { user: ["set-role"] }),
   validate(adminValidations.SetUserRoleRequestSchema),
   adminController.setUserRole
 );
 
 userRoutes.patch(
   "/password",
-  auth(["admin"], { user: ["set-password"] }),
+  auth([...adminRoles], { user: ["set-password"] }),
   validate(adminValidations.SetUserPasswordRequestSchema),
   adminController.setUserPassword
 );
 
 userRoutes.patch(
   "/ban",
-  auth(["admin"], { user: ["ban"] }),
+  auth([...adminRoles], { user: ["ban"] }),
   validate(adminValidations.BanUserRequestSchema),
   adminController.banUser
 );
 
 userRoutes.patch(
   "/unban",
-  auth(["admin"], { user: ["ban"] }),
+  auth([...adminRoles], { user: ["ban"] }),
   validate(adminValidations.UnbanUserRequestSchema),
   adminController.unbanUser
 );
 
 userRoutes.get(
   "/sessions",
-  auth(["admin"], { session: ["list"] }),
+  auth([...adminRoles], { session: ["list"] }),
   validate(adminValidations.ListUserSessionsRequestSchema),
   adminController.listUserSession
 );
 
 userRoutes.delete(
   "/sessions",
-  auth(["admin"], { session: ["revoke"] }),
+  auth([...adminRoles], { session: ["revoke"] }),
   validate(adminValidations.RevokeAllUserSessionRequestSchema),
   adminController.revokeAllUserSessions
 );
 
 userRoutes.delete(
   "/sessions/:sessionToken",
-  auth(["admin"], { session: ["revoke"] }),
+  auth([...adminRoles], { session: ["revoke"] }),
   validate(adminValidations.RevokeUserSessionRequestSchema),
   adminController.revokeUserSession
 );
 
 userRoutes.post(
   "/impersonate",
-  auth(["admin"], { user: ["impersonate"] }),
+  auth([...adminRoles], { user: ["impersonate"] }),
   validate(adminValidations.ImpersonateUserRequestSchema),
   adminController.impersonateUser
 );
 
 userRoutes.delete(
   "/impersonate",
-  auth(["admin"], { user: ["impersonate"] }),
+  auth([...adminRoles], { user: ["impersonate"] }),
   validate(adminValidations.StopImpersonatingRequestSchema),
   adminController.stopImpersonatingUser
 );
@@ -89,12 +91,12 @@ router.use("/users/:userId", userRoutes);
 router
   .route("/users")
   .get(
-    auth(["admin"], { user: ["list"] }),
+    auth([...adminRoles], { user: ["list"] }),
     validate(adminValidations.ListUserRequestSchema),
     adminController.listUsers
   )
   .post(
-    auth(["admin"], { user: ["create"] }),
+    auth([...adminRoles], { user: ["create"] }),
     validate(adminValidations.CreateUserRequestSchema),
     adminController.createUser
   );
--- src/routes/v1/user.route.ts
+++ src/routes/v1/user.route.ts
@@ -1,15 +1,17 @@
 import express, { type Router } from "express";
-import userController from "../../controllers/user.controller.js";
-import userValidations from "../../validations/user.validations.js";
+import { userController } from "../../controllers/index.js";
+import { userValidations } from "../../validations/index.js";
 import validate from "../../middleware/validate.js";
 import auth from "../../middleware/auth.js";
+
+import { defaultRoles, adminRoles } from "@/lib/permissions.ts";
 
 const router: Router = express.Router();
 
 router
   .route("/self")
   .get(
-    auth(["user"]),
+    auth([...defaultRoles, ...adminRoles]),
     validate(userValidations.getUserWithSession),
     userController.getUserWithSession
   );
@@ -17,7 +19,7 @@ router
 router
   .route("/:id")
   .get(
-    auth(["user"], { user: ["listPublicUsers"] }),
+    auth([...defaultRoles, ...adminRoles], { publicUser: ["listPublicUsers"] }),
     validate(userValidations.getUserById),
     userController.getPublicUserById
   );
@@ -25,7 +27,7 @@ router
 router
   .route("/")
   .get(
-    auth(["user"], { user: ["listPublicUsers"] }),
+    auth([...defaultRoles, ...adminRoles], { publicUser: ["listPublicUsers"] }),
     validate(userValidations.listPublicUsers),
     userController.listPublicUsers
   );
